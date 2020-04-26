e6584b9730368f09a40d0f562c5ecfa7afdef4e9
RyanFelt <ryanwfeltkamp@gmail.com>
2020-04-26 14:46:14 -0400

    added infinit scrolling to home page

--- front-end/src/components/pages/Home.js
+++ front-end/src/components/pages/Home.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import '../../App.css';
 import { useLocation } from 'react-router';
+import InfiniteScroll from 'react-infinite-scroller';
+import { Spinner } from 'react-bootstrap';
 import qs from 'query-string';
 import { LoadingSpinner } from '../lib/LoadingSpinner';
 import { TitleTile } from '../lib/TitleTile';
@@ -11,39 +13,74 @@ export const Home = () => {
   const { filter } = qs.parse(search);
 
   const [titles, setTitles] = useState([]);
+  const [visibleTitles, setVisibleTitles] = useState([]);
+  const [numVisibleTitles, setNumVisibleTitles] = useState(0);
 
   useEffect(() => {
-    if (!titles.length) {
-      getAllTitles().then(res => {
+    if (!visibleTitles.length) {
+      getAllTitles().then((res) => {
         let titlesData = res;
 
         if (filter) {
-          titlesData = res.filter(item => {
+          titlesData = res.filter((item) => {
             return filter === item.type.toLowerCase();
           });
         }
+
+        const latestNumVisibleTitles =
+          titlesData.length > 5 ? 5 : titlesData.length;
+        setNumVisibleTitles(latestNumVisibleTitles);
+
         setTitles(titlesData);
+
+        let titlesToAdd = [];
+        for (let x = 0; x < latestNumVisibleTitles; x++) {
+          titlesToAdd.push(titlesData[x]);
+        }
+        setVisibleTitles(titlesToAdd);
       });
     }
-  }, [titles]);
+  }, []);
+
+  const loadMoreTitles = () => {
+    const maxNumVisibleTitles =
+      titles.length > numVisibleTitles + 5
+        ? numVisibleTitles + 5
+        : titles.length;
+
+    let addTitles = [];
+    for (let x = numVisibleTitles; x < maxNumVisibleTitles; x++) {
+      addTitles.push(titles[x]);
+    }
+
+    setVisibleTitles([...visibleTitles, ...addTitles]);
+    setNumVisibleTitles(numVisibleTitles + 5);
+  };
 
   return (
     <div className="App">
-      {titles && titles.length ? (
-        <div className="flex-column">
-          {titles.map(title => {
-            return (
-              <TitleTile
-                path={title.title}
-                image={title.bannerImage}
-                imageLocation={title.bannerImageLocation}
-                type={title.type}
-                videoFile={title.videoFile}
-                key={title.id}
-              />
-            );
-          })}
-        </div>
+      {visibleTitles.length ? (
+        <InfiniteScroll
+          loadMore={loadMoreTitles}
+          hasMore={visibleTitles.length !== titles.length}
+          loader={<Spinner animation="border" />}
+          threshold={50}
+        >
+          <div className="flex-column">
+            {visibleTitles.map((title) => {
+              return (
+                <TitleTile
+                  path={title.title}
+                  image={title.bannerImage}
+                  imageLocation={title.bannerImageLocation}
+                  type={title.type}
+                  videoFile={title.videoFile}
+                  key={title.id}
+                />
+              );
+            })}
+          </div>
+        </InfiniteScroll>
       ) : (
         <LoadingSpinner />
       )}
