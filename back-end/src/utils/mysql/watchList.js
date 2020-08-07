0f33e47a2340d087ff30802912d6d0adc4f887c7
RyanFelt <ryanwfeltkamp@gmail.com>
2020-08-07 11:31:13 -0400

    added history to watchlist api response

--- back-end/src/utils/mysql/watchList.js
+++ back-end/src/utils/mysql/watchList.js
@@ -2,14 +2,16 @@ const moment = require('moment');
 const { query } = require('./setup');
 const { ValidationError } = require('../errors');
 
-const { WATCH_LIST_TABLE, TITLES_TABLE } = process.env;
+const { WATCH_LIST_TABLE, TITLES_TABLE, WATCH_HISTORY_TABLE } = process.env;
 
 exports.getWatchList = async (user_id) => {
   try {
     return await query(`
-      SELECT ${TITLES_TABLE}.* FROM ${WATCH_LIST_TABLE} 
+      SELECT ${TITLES_TABLE}.*, ${WATCH_HISTORY_TABLE}.watched_percentage FROM ${WATCH_LIST_TABLE} 
       LEFT JOIN ${TITLES_TABLE} ON  ${WATCH_LIST_TABLE}.title_id = ${TITLES_TABLE}.id
-      WHERE user_id = '${user_id}'
+      LEFT JOIN ${WATCH_HISTORY_TABLE}
+      ON ${TITLES_TABLE}.id = ${WATCH_HISTORY_TABLE}.title_id AND "${user_id}" = ${WATCH_HISTORY_TABLE}.user_id
+      WHERE ${WATCH_LIST_TABLE}.user_id = '${user_id}'
       ORDER BY date_added desc;
     `);
   } catch (err) {
